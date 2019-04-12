9ed4811d535065f6ea5f5f1ce8edb596ac40d476
jenbeltran <francescajenbeltran@gmail.com>
2019-04-12 12:01:56 -0400

    passport js

---
+++ src/authentication.js
@@ -0,0 +1,1 @@
+//page to separate passport routes?
--- src/router.js
+++ src/router.js
@@ -3,34 +3,103 @@
 
 //connecting the required libraries etc.
 let express = require('express'),
+	session = require('express-session'),
 	router = express.Router(),
+	bodyParser = require('body-parser'),
 	sqlite3 = require('sqlite3').verbose(),
 	db = new sqlite3.Database('database/books.sqlite'),
-	bodyParser = require('body-parser'),
+	passport = require('passport'),
+	LocalStrategy = require('passport-local').Strategy,
 	urlencodedParser = bodyParser.urlencoded({ extended: false }),
-	methodOverride = require('method-override');
+	methodOverride = require('method-override'),
+	cookieParser = require('cookie-parser'),
+	flash = require('connect-flash'),
+	passportMiddleWare = require('../middleware/passport');
 
+// TODO: figure out why passport middleware is not connecting
+
+router.use(cookieParser);
+
+router.use(
+	session({
+		secret            : 'justasecret',
+		resave            : true,
+		saveUninitialized : true
+	})
+);
+
+//Passport config
+router.use(passport.initialize());
+router.use(passport.session());
+
+//Flash
+router.use(flash());
+
+// to use PUT and DELETE requests
 router.use(methodOverride('_method'));
 
 ///////////////////////// AUTHENTICATION ROUTES //////////////////////////
 //REGISTER ROUTE
+//NEW route - shows the register form
 router.get('/register', (req, res, err) => {
-	try {
-		res.render('authentication/register');
-	} catch (err) {
-		res.render('errorPage');
-	}
+	res.render('authentication/register', { message: req.flash('signupMessage') });
 });
-//TODO: add the user to the db, then continue to secret INDEX page
+
+//CREATE route - creates the new user and redirects to all books page
+// router.post('/register', urlencodedParser, (req, res) => {
+// 	bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+// 		db.run(
+// 			'INSERT INTO users(username, password, user_fname, user_lname, email) VALUES(?, ?, ?, ?, ?)',
+// 			[ req.body.username, hash, req.body.user_fname, req.body.user_lname, req.body.email ],
+// 			(err) => {
+// 				if (err) next(err);
+// 				else {
+// 					res.redirect('/books');
+// 				}
+// 			}
+// 		);
+// 	});
+// });
+
+router.post(
+	'/register',
+	passport.authenticate('local-signup', {
+		successRedirect : '/books',
+		failureRedirect : '/register',
+		failureFlash    : true
+	})
+);
+
+//TODO: ensure passwords match, hide register button and login buttons on ejs
 
 // LOGIN ROUTE
+//NEW route - shows the login form
 router.get('/login', (req, res, err) => {
-	try {
-		res.render('authentication/login');
-	} catch (err) {
-		res.render('errorPage');
-	}
+	res.render('authentication/login', { message: req.flash('loginMessage') });
 });
+
+//TODO: redirect to book page AND authenticate the hash password
+//POST route - confirms the user's info and redirects to secret page
+
+router.post(
+	'/login',
+	passport.authenticate('local-login', {
+		successRedirect : '/books',
+		failureRedirect : '/login',
+		failureFlash    : true
+	}),
+	function(req, res) {
+		if (req.body) {
+			req.session.cookie.maxAge = 1000 * 60 * 3;
+		} else {
+			req.session.cookie.expires = false;
+		}
+		// If this function gets called, authentication was successful.
+		// `req.user` contains the authenticated user.
+		res.redirect('/books');
+	}
+);
+
 //TODO: check if user is in db, if so, then continue to secret INDEX page
 //if not, then refresh the login page
 
